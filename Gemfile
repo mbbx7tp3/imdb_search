a91015efdddb894db4437f390fd13841a4bb76c3
Tom Poulter <tom.poulter.1@outlook.com>
2020-02-10 21:36:14 +0000

    playing about with searching

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'pg', '~> 0.21'
 gem 'puma'
 gem 'rails', '5.2.4.1'
 gem 'redis'
+gem 'pg_search'
 
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
@@ -14,6 +15,7 @@ gem 'sassc-rails'
 gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
+gem 'searchkick', '~> 3.0'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,16 @@
 class Movie < ApplicationRecord
-  belongs_to :director
+   belongs_to :director
+   searchkick
+  #include PgSearch::Model
+  #pg_search_scope :global_search,
+    #against: [ :title, :syllabus ],
+    #associated_against: {
+      #director: [ :first_name, :last_name ]
+    #},
+    #using: {
+      #tsearch: { prefix: true }
+    #}
+
+  #multisearchable against: [:title, :syllabus]
+
 end
